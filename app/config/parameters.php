d9c44244907fc9dcf70154ed2abfe4b6f9e06d0f
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-07-16 00:06:25 +0300

    COnfigure via a .php file

---
+++ app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php
+
+// Database Settings
+$container->setParameter('database_host', getenv("SYMFONY__database_host"));
+$container->setParameter('database_port', getenv("SYMFONY__database_port"));
+$container->setParameter('database_name', getenv("SYMFONY__database_name"));
+$container->setParameter('database_user', getenv("SYMFONY__database_user"));
+$container->setParameter('database_password', getenv("SYMFONY__database_password"));
+
+
+//Smtp Settings
+$container->setParameter('mailer_transport', 'smtp');
+$container->setParameter('mailer_host', getenv('SYMFONY__smtp_host'));
+
+$useSmtpSSL=getenv('SYMFONY__smtp_use_ssl');
+$useSmtpSSL=filter_var($useSmtpSSL, FILTER_VALIDATE_BOOLEAN);
+if($useSmtpSSL){
+ $container->setParameter('mailer_encryption_method', 'ssl');
+}
+
+$container->setParameter('mailer_port', getenv('SYMFONY__smtp_port'));
+$container->setParameter('mailer_user', getenv('SYMFONY__smtp_user'));
+$container->setParameter('mailer_password', getenv('SYMFONY__smtp_password'));
+
+$container->setParameter('secret','ncrwoe3398hxujiqwbhdslasTT^ebghuikas');
