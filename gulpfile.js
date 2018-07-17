7527ac4ece19b2dc3dc97d1ca064d53277b19f8b
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-07-17 17:41:25 +0300

    Basic Frotnend Tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,56 @@
-var gulp= require('gulp');
-var rename=require('gulp-rename');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
 var ext_replace = require('gulp-ext-replace');
+var argv = require('yargs').boolean('production').alias('p','production').argv;
 
 /**
 * Location where to store the 3rd party libraries
 */
-var web_folder="web/assets/vendor"
+var frontend_folder="./frontend"
+var vendor_folder=`${frontend_folder}/vendor`
+var frontent_dev_folder_js=`${frontend_folder}/js`
+var frontent_dev_folder_js=`${frontend_folder}/css`
 
+var dev= !argv.p;
 
 /*################################### Installing Dependencies ###############################*/
 
 //Move Bootstrap
 gulp.task('move_bootstrap',function(){
 
-  var bootstrap_dir='bower_components/AdminLTE/bootstrap';
-  var dest=web_folder+'/bootstrap';
-  //MoveCss
-  gulp.src(bootstrap_dir+'/css/bootstrap.min.css')
-    .pipe(rename('bootstrap.css'))
-    .pipe(gulp.dest(dest+'/css'))
+  var bootstrap_dir='./node_modules/bootstrap/dist';
+  var dest=`${vendor_folder}/bootstrap`;
 
-  gulp.src(bootstrap_dir+'/js/bootstrap.min.js')
-    .pipe(rename('bootstrap.js'))
-    .pipe(gulp.dest(dest+'/js'));
+  var css_files=[`${bootstrap_dir}/css/bootstrap.min.css`,`${bootstrap_dir}/css/bootstrap.min.css.map`];
+  var js_files=[`${bootstrap_dir}/js/bootstrap.bundle.min.js`,`${bootstrap_dir}/js/bootstrap.bundle.min.js.map`];
 
-  gulp.src(bootstrap_dir+'/fonts/*')
-      .pipe(gulp.dest(dest+'/fonts'));
+  gulp.src(css_files).pipe(gulp.dest(`${dest}/css`));
+  gulp.src(js_files).pipe(gulp.dest(`${dest}/js`));
+
 })
 
 //Jquery & miscellanous Javascript move
 gulp.task('move_jquery',function(){
 
   var jqueryFiles=[
-                    'bower_components/jquery/dist/jquery.min.js',
-                    'bower_components/jquery-slimscroll/jquery.slimscroll.min.js',
-                    'bower_components/jquery-ui/jquery-ui.min.js',
-                    'bower_components/fastclick/lib/fastclick.js'
+                    './node_modules/jquery/dist/jquery.min.js',
                   ]
-  gulp.src(jqueryFiles)
-    .pipe(ext_replace('.js','.min.js'))
-    .pipe(gulp.dest(web_folder))
+
+  gulp.src(jqueryFiles).pipe(gulp.dest(vendor_folder))
 });
 
-//Move the Admin's LTE css
-gulp.task('move_admin_lte',function(){
-
-  var cssLocation='bower_components/AdminLTE/dist/css';
-  var jsLocaton="bower_components/AdminLTE/dist/js/"
-  var dest=web_folder+'/adminlte/'
-
-  gulp.src([cssLocation+'/AdminLTE.min.css'])
-    .pipe(rename('adminlte.css'))
-    .pipe(gulp.dest(dest));
-
-  gulp.src([cssLocation+'/skins/skin-blue.min.css'])
-      .pipe(rename('skin-blue.css'))
-      .pipe(gulp.dest(dest));
-  
-  gulp.src(jsLocaton+"app.min.js").pipe(gulp.dest(dest));
-});
-
-//For xeditable library
-gulp.task('move_xeditable',function(){
-  var path='bower_components/x-editable/dist/bootstrap3-editable';
-  var dest=web_folder+'/xeditable';
-
-  //Move Xeditable library itself
-  gulp.src(path+'/css/*').pipe(gulp.dest(dest+'/css'));
-  gulp.src(path+'/img/*').pipe(gulp.dest(dest+'/img'));
-  gulp.src(path+'/js/bootstrap-editable.min.js').pipe(rename('xeditable.js')).pipe(gulp.dest(dest+'/js/'))
-})
 
 //For fontawesome
 gulp.task('move_fontawesome',function(){
-  var path='bower_components/font-awesome';
-  var dest=web_folder+'/font-awesome';
+  var path='./node_modules/font-awesome';
+  var dest=vendor_folder+'/font-awesome';
 
   gulp.src(path+'/fonts/*').pipe(gulp.dest(dest+'/fonts'));
-  gulp.src(path+'/css/font-awesome.min.css')
-    .pipe(rename('font-awesome.css'))
-    .pipe(gulp.dest(dest+'/css'));
+  gulp.src(path+'/css/font-awesome.min.css').pipe(gulp.dest(dest+'/css'));
 });
 
-//For jqueryfileupload and knockout bindings
-gulp.task('move_jquery_fileupload',function(){
-  var src='bower_components/blueimp-file-upload/js/'
-
-  var jquery_files=[src+'jquery.fileupload.js',src+'jquery.iframe-transport.js',src+'vendor/jquery.ui.widget.js']
-  var dest=web_folder+'/jquery_fileupload/'
-  gulp.src(jquery_files).pipe(gulp.dest(dest));
-
-});
-
-gulp.task('move_icheck',function(){
-	var css='bower_components/iCheck/skins/*/*'
-	var js='bower_components/iCheck/icheck.min.js'
-	var dest=web_folder+'/icheck'
-	
-	gulp.src(js).pipe(rename('icheck.js')).pipe(gulp.dest(dest))
-	gulp.src(css).pipe(gulp.dest(dest))
-})
 /* ############################################ Installing Dependencies ##################################### */
 
-gulp.task('move_frontend',['move_bootstrap','move_admin_lte','move_jquery','move_xeditable','move_fontawesome','move_jquery_fileupload','move_icheck']);
+gulp.task('move_frontend',['move_bootstrap','move_jquery']);
 
 gulp.task('default',['move_frontend']);
