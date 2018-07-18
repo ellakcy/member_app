90d9e49e34a00336ff7093cc1a4df68bf2e803ce
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-07-18 19:17:27 +0300

    Registration form Form depending user show element

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -6,10 +6,22 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-
+use Symfony\Component\Validator\Constraints\IsTrue as TrueConstraint;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
 class RegistrationType extends AbstractType
 {
+
+    /**
+    * @var AuthorizationChecker
+    */
+    private $authorizationChecker=null;
+
+    public function __construct(AuthorizationChecker $authorizationChecker)
+    {
+      $this->authorizationChecker=$authorizationChecker;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name',TextType::class,["label"=>"register.name","required"=>true,'translation_domain' => 'FOSUserBundle'])
@@ -17,6 +29,12 @@ class RegistrationType extends AbstractType
                 ->add('phone',PhoneNumberType::class,["label"=>"register.phonenum",'translation_domain' => 'FOSUserBundle'])
                 ->add('organization',TextType::class,["label"=>"register.organization","required"=>false,'translation_domain' => 'FOSUserBundle'])
                 ->add('occupation',TextType::class,["label"=>"register.position","required"=>false,'translation_domain' => 'FOSUserBundle']);
+
+        if(!$this->authorizationChecker->isGranted('ROLE_ADMIN'))
+        {
+          $builder->add('accept_terms',CheckboxType::class,["label"=>"register.acceptTerms","required"=>true,'translation_domain' => 'FOSUserBundle',
+                                                            'mapped' => false,'constraints' => new TrueConstraint(array('message' => 'Your Confirmation Message','groups' => 'Registration'))]);
+        }
     }
 
     public function getParent()
