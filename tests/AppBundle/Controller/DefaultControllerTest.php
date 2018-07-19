0bbfbfe563161e07d768270cb17f425fc6d84655
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-07-19 19:00:34 +0300

    First Test work

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -11,8 +11,8 @@ class DefaultControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $response=$client->getResponse();
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login',$response->headers->get('Location'));
     }
 }
