7021f1502e6af41feecb1fd7e64e5c5a49af878c
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-08-06 15:08:57 +0300

    Loading the fontawesome better

--- gulpfile.js
+++ gulpfile.js
@@ -37,11 +37,11 @@ gulp.task('move_jquery',function(done){
 
 //For fontawesome
 gulp.task('move_fontawesome',function(done){
-  var path='./node_modules/@fortawesome/fontawesome-free-webfonts';
+  var path='./node_modules/@fortawesome/fontawesome-free';
   var dest=vendor_folder+'/font-awesome';
 
   gulp.src(path+'/webfonts/*').pipe(gulp.dest(dest+'/webfonts'));
-  gulp.src(path+'/css/fontawesome.css').pipe(gulp.dest(dest+'/css'));
+  gulp.src(path+'/css/all.min.css').pipe(gulp.dest(dest+'/css'));
 
   done();
 });
