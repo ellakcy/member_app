b4af1987dd82fed68256a06ed2d03ff657e9570e
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-08-17 21:33:47 +0300

    Add contact emails

---
+++ src/AppBundle/Entity/ContactEmail.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * ContactEmail
+ *
+ * @ORM\Table(name="contact_email")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContactEmailRepository")
+ * @UniqueEntity("email")
+ */
+class ContactEmail
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+    * @var String
+    * @ORM\Column(name="email",type="string",unique=true)
+    * @Assert\Email( message = "The email '{{ value }}' is not a valid email.")
+    */
+    private $email;
+
+    /**
+    * @ORM\Column(name="date", type="datetime", options={"default"="CURRENT_TIMESTAMP"})
+    */
+    private $createdAt;
+
+    /**
+     * Get id
+     *
+     * @return int
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+    * @param String $email
+    * @return ContactEmail
+    */
+    public function setEmail($email){
+      $this->email=$email;
+
+      return $this;
+    }
+
+    /**
+    * @return String
+    */
+    public function getEmail(){
+      return $this->email;
+    }
+}
