8c7c42705dc63b2dced08709245d1545f6aac8ad
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-08-18 14:13:43 +0300

    Add some code for better bootstrapping on vegrant setup

--- after.sh
+++ after.sh
@@ -3,3 +3,17 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+cd /home/vagrant/code
+
+echo "Clearing caches for dev environment"
+php ./bin/console cache:clear
+php ./bin/console cache:warmup
+
+echo "Clearing caches for test environment"
+php ./bin/console cache:clear --env=test
+php ./bin/console cache:warmup --env=test
+
+echo "Creatinc schemas for test environment"
+php ./bin/console doctrine:schema:drop --env=test --force
+php ./bin/console doctrine:schema:create --env=test
