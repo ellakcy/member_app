6ca9a9d9b1174821971ebff10803dda43210b973
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-09-01 22:13:50 +0300

    Get Repository

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Gregwar\Captcha\CaptchaBuilder;
 
 class DefaultController extends Controller
@@ -28,9 +29,11 @@ class DefaultController extends Controller
 
     /**
     * @Route("registration/email", name="registration_email_contact")
-    * @Method("POST")
     */
-    public function addEmailAction(Request $request){
+    public function addEmailAction(Request $request)
+    {
+      $contactEmail=$this->get('doctrine.orm.entity_manager')->getRepository('AppBundle:ContactEmail');
 
+      return new Response("Hello");
     }
 }
