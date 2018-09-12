67ebd74da46edf8287beb98a5b9cc964b6fb90d3
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-09-12 20:56:11 +0300

    Autoconfigure xdebug.ini

--- after.sh
+++ after.sh
@@ -14,6 +14,19 @@ echo "Clearing caches for test environment"
 php ./bin/console cache:clear --env=test
 php ./bin/console cache:warmup --env=test
 
-echo "Creatinc schemas for test environment"
+echo "Creating schemas for test environment"
 php ./bin/console doctrine:schema:drop --env=test --force
 php ./bin/console doctrine:schema:create --env=test
+
+echo "Configuring Xdebug"
+ip=$(hostname -I | awk '{print $2}')
+xdebug_config="/etc/php/$(php -v | head -n 1 | awk '{print $2}'|cut -c 1-3)/mods-available/xdebug.ini"
+
+echo "IP for the xdebug to connect back: ${ip}"
+echo "Xdebug Configuration path: ${xdebug_config}"
+echo "Port for the Xdebug to connect back: ${XDEBUG_PORT}"
+echo "Optimize for ${IDE} ide"
+
+if [ $IDE=='atom' ]; then
+  echo "Configuring xdebug for ATOM ide"
+fi
