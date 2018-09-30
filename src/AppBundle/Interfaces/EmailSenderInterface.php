e6ed2b5f4a482f8a16d61355a58cbeeb1e456bc5
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-09-30 22:52:33 +0300

    Basic Interface to Send Emails

--- src/AppBundle/Interfaces/EmailSenderInterface.php
+++ src/AppBundle/Interfaces/EmailSenderInterface.php
@@ -6,6 +6,17 @@ namespace AppBundle\Interfaces\EmailSenderInterface;
 */
 interface EmailSender
 {
-
-  public function send($from,$to,$body_plain,$body_html,array $cc=[],array $bcc=[]);
+  /**
+  * Basic function to send emails
+  * @param String $from Sender's email address where the email will be sent
+  * @param String $to Receipient email address
+  * @param String $bodyPlain The plaintext body of the email
+  * @param String $bodyHtml The html formated body
+  * @param String $title The Email's title
+  * @param Array $cc The list of Carbon Copy conotified email addresses
+  * @param Array $bcc The list of Blind Carbon Copy conotified email addresses
+  *
+  * @return boolean Indicating whether the email has been sent or not
+  */
+  public function send($from,$to,$bodyPlain="",$bodyHtml="",$title="",array $cc=[],array $bcc=[]);
 }
