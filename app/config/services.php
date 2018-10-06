0a12e67b8afb9fed79be20f7898e4b099280134c
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-10-06 12:24:19 +0300

    use php for service configuration

---
+++ app/config/services.php
@@ -0,0 +1,33 @@
+<?php
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+// To use as default template
+$definition = new Definition();
+
+$definition->setAutowired(true)->setAutoconfigured(true)->setPublic(false);
+
+// $this is a reference to the current loader
+$this->registerClasses($definition, 'AppBundle\\', '../../src/AppBundle/*', '../../src/AppBundle/{Entity,Repository,Tests,Interfaces,Services/Adapters/RepositoryServiceAdapter.php}');
+
+$definition->addTag('controller.service_arguments');
+$this->registerClasses($definition, 'AppBundle\\Controller\\', '../../src/AppBundle/Controller/*');
+
+$container->register(AppBundle\Services\CapthaServiceAdapter::class)
+  ->setPublic(true)
+  ->setArguments([new Reference('session')]);
+
+$container->register(AppBundle\Services\NormalEmailSend::class)
+  ->setArguments([new reference('mailer')]);
+
+
+/*######################## Logging ################################*/
+$container->register('ellakcy.log.formatter.access_log',Monolog\Formatter\LineFormatter::class)
+  ->setArguments(["%%message%% \n"]);
+
+
+/*############################ Event Listeners ###################*/
+$container->register(AppBundle\EventListener\AccessLogEventListener::class)
+  ->setArgument('$logger', new Reference('monolog.logger.access_log'))
+  ->setArgument('$twig', new Reference('twig'))
+  ->addtag('kernel.event_listener',['event'=>'kernel.response','method'=>'onKernelResponse']);
