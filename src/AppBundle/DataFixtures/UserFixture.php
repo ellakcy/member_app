2a6b313e93c1345683a29566e79c29615916a028
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-10-15 00:14:39 +0300

    Load User with fixtures

--- src/AppBundle/DataFixtures/UserFixture.php
+++ src/AppBundle/DataFixtures/UserFixture.php
@@ -3,12 +3,62 @@ namespace AppBundle\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use AppBundle\Etities\User;
+use AppBundle\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
  class UserFixture extends Fixture
  {
+
+   /**
+   * @var UserPasswordEncoderInterface
+   */
+   private $passwordEncoder=null;
+
+   public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+   {
+     $this->passwordEncoder=$passwordEncoder;
+   }
+
+
+   public function createSimpleActivatedUser(ObjectManager $manager)
+   {
+     $activeUser= new User('jdoe','jdoe@example.com',['ROLE_USER'],true);
+     $activeUserPassword=$this->passwordEncoder->encodePassword($activeUser,'pass1234');
+     $activeUser->setPassword($activeUserPassword);
+
+     $manager->persist($activeUser);
+   }
+
+   public function createInactiveAUserWithPassword(ObjectManager $manager)
+   {
+     $inActiveUser= new User('jdoe_inactive','jdoeInactive@example.com',['ROLE_USER'],false);
+     $activeUserPassword=$this->passwordEncoder->encodePassword($inActiveUser,'pass1234');
+     $inActiveUser->setPassword($activeUserPassword);
+
+     $manager->persist($inActiveUser);
+   }
+
+   public function createInactiveAUserWithoutPassword(ObjectManager $manager)
+   {
+     $inActiveUser= new User('paswordless','paswordless@example.com',['ROLE_USER'],false);
+     $manager->persist($inActiveUser);
+   }
+
+   public function createAdmin(ObjectManager $manager)
+   {
+     $activeAdmin= new User('admin','elakcyAdmin@example.com',['ROLE_USER'],true);
+     $activeUserPassword=$this->passwordEncoder->encodePassword($activeAdmin,'admin1234');
+     $activeAdmin->setPassword($activeUserPassword);
+     $manager->persist($activeAdmin);
+   }
+
    public function load(ObjectManager $manager)
    {
+     $this->createSimpleActivatedUser($manager);
+     $this->createInactiveAUserWithPassword($manager);
+     $this->createInactiveAUserWithoutPassword($manager);
+     $this->createAdmin($manager);
 
+     $manager->flush();
    }
  }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -46,28 +46,70 @@ class User implements UserInterface, \Serializable
     private $roles = [];
 
 
-    public function __construct()
+    /**
+    * @param String $displayName How the user will be viewed to other users (NOT USED for LOGIN)
+    * @param String $email The User's Email
+    */
+    public function __construct($displayName=null, $email=null, array $roles=[], $isActive=false)
     {
-        $this->isActive = false;
-        // may not be needed, see section on salt below
-        // $this->salt = md5(uniqid('', true));
+        $this->isActive = $isActive;
+        $this->setUsername($displayName);
+        $this->setEmail($email);
+        $this->setRoles($roles);
+    }
+
+    /**
+    * @param String $displayName How the user will be viewed to other users (NOT USED for LOGIN)
+    * @return User
+    */
+    public function setDisplayName(string $displayName)
+    {
+      $this->displanName=$displayName;
+      return $this;
+    }
+
+    /**
+    * Alias to setDisplayName method.
+    * @param String $username How the user will be viewed to other users (NOT USED for LOGIN)
+    * @return User
+    */
+    public function setUsername($username)
+    {
+      return $this->setDisplayName($username);
     }
 
     public function getUsername()
     {
-        return $this->displayName;
-    }
-
-    public function getSalt()
-    {
-        // you *may* need a real salt depending on your encoder
-        // see section on salt below
-        return null;
+      return !empty($this->displayName)?$this->displayName:$this->email;
     }
 
     public function getPassword()
     {
         return $this->password;
+    }
+
+    public function setEmail($email)
+    {
+      $this->email=$email;
+      return $this;
+    }
+
+    /**
+    * @return String
+    */
+    public function getEmail()
+    {
+      return $this->email;
+    }
+
+    /**
+    * Set an ENCODED (Cryptographically Hashed) password to this user
+    * @param String $password CRYPTOGRAPHICALLY Hashed password
+    */
+    public function setPassword(string $password)
+    {
+      $this->password=$password;
+      return $this;
     }
 
     public function getRoles()
@@ -77,21 +119,40 @@ class User implements UserInterface, \Serializable
       if (!in_array('ROLE_USER', $roles)) {
         $roles[] = 'ROLE_USER';
       }
+
+      // Append extra autoset logic here
+
       return $roles;
+    }
+
+    /**
+    * Adds an extra role to the user
+    * @return User
+    */
+    public function appendRole(string $role)
+    {
+      if (!in_array($role, $this->roles)) {
+        $this->roles[]=$role;
+      }
+
+      return $this;
+    }
+
+    /**
+     * OVERRIDES the user roles
+     * @param Array $roles The array of the roles to get overriden
+     * @return User
+     */
+    public function setRoles(array $roles)
+    {
+      $this->roles=$roles;
+
+      return $this;
     }
 
     public function eraseCredentials()
     {
-    }
-
-    public function isAccountNonExpired()
-    {
-      return true;
-    }
-
-    public function isAccountNonLocked()
-    {
-      return true;
+      $this->password="";
     }
 
     public function isCredentialsNonExpired()
@@ -104,16 +165,44 @@ class User implements UserInterface, \Serializable
           return $this->isActive && !empty($this->password);
     }
 
+    public function enable()
+    {
+      $this->isActive=true;
+      return $this;
+    }
+
+    public function disable()
+    {
+      $this->isActive=false;
+      return $this;
+    }
+
+    // Methods with default actions
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function isAccountNonExpired()
+    {
+      return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+      return true;
+    }
 
     /** @see \Serializable::serialize() */
     public function serialize()
     {
         return serialize(array(
             $this->id,
-            $this->$displayName,
+            $this->displayName,
             $this->password,
             $this->email,
             $this->isActive,
+            $this->roles
             // see section on salt below
             // $this->salt,
         ));
@@ -124,10 +213,11 @@ class User implements UserInterface, \Serializable
     {
         list (
             $this->id,
-            $this->$displayName,
+            $this->displayName,
             $this->password,
             $this->email,
             $this->isActive,
+            $this->roles
             // see section on salt below
             // $this->salt
         ) = unserialize($serialized, array('allowed_classes' => false));
