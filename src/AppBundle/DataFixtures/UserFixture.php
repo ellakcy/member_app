57434037f2ec852dccb537dec925ac763c3897a8
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-10-14 22:57:05 +0300

    Fixture to load users

---
+++ src/AppBundle/DataFixtures/UserFixture.php
@@ -0,0 +1,14 @@
+<?php
+namespace AppBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Etities\User;
+
+ class UserFixture extends Fixture
+ {
+   public function load(ObjectManager $manager)
+   {
+
+   }
+ }
